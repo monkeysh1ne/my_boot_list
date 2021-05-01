733ec0b62e0862e6d9f6d62b4421d512e2f33af6
Darrell Mayson <mcdoob@gmail.com>
2021-05-02 09:03:43 +1200

    start coding book, event & UI clases

--- app.js
+++ app.js
@@ -8,10 +8,47 @@ class Book {
 }
 
 // UI Class: Handle UI Tasks
+class UI {
+  static displayBooks(){
+    // hard coded values for testing only
+    const StoredBooks = [
+      {
+        title: 'Book One',
+        author: 'John Doe',
+        isbn: 365467
+      },
+      {
+        title: 'Book Two',
+        author: 'Jane Doe',
+        isbn: 9877543
+      }
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book){
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+}
 
 // Store Class: Handles Storage
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
 

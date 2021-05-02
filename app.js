91875b81c34bd55978885829b6842dd34a2a4d4d
Darrell Mayson <mcdoob@gmail.com>
2021-05-03 07:44:04 +1200

    added validation and alert in UI class

--- app.js
+++ app.js
@@ -53,6 +53,19 @@ class UI {
     }
   }
 
+  static showAlert(message, className){
+    // dynamically create a ne div to insert into DOM for displaying UI messages
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+
+    // Vanish alerts after 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  }
+
 
   static clearFields(){
     document.querySelector('#title').value = '';
@@ -79,19 +92,26 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
 
-  // Instantiate book
-  const book = new Book(title, author, isbn);
+  // Validate
+  if(title === '' || author === '' || isbn === ''){
+    UI.showAlert('Please fill in all the fields.', 'danger');
 
-  // Add book to UI
-  UI.addBookToList(book);
+  } else {
+    // Instantiate book
+    const book = new Book(title, author, isbn);
 
-  // Clear form fields
-  UI.clearFields();
+    // Add book to UI
+    UI.addBookToList(book);
+
+    // Clear form fields
+    UI.clearFields();
+    }
+
 
 
 });
 
-// Event: Remove a Book
+// Event: Remove a Book NB: targets tbody element #book-list to ensure removing ROW not just delete anchor!
 document.querySelector('#book-list').addEventListener('click', (e) => {
   UI.deleteBook(e.target);
 })
